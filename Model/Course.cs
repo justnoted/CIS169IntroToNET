0d27d4acec4d6563b1037c8cac0caf15e35131ff
Noted <just.noted3505@gmail.com>
2024-05-09 11:54:07 -0500

    completed assignment

--- Model/Course.cs
+++ Model/Course.cs
@@ -1,13 +1,14 @@
 using System.ComponentModel.DataAnnotations;
+using System.Configuration;
 
 namespace CIS169IntroToNET.Model;
 
 public class Course
 {
     public int Id { get; set; }
-    public string? CourseName { get; set; }
-    public string? CourseDescription { get; set; }
-    public int RoomNumber { get; set; }
-
-    [DataType(DataType.Time)] public TimeOnly StartTime { get; set; }
+    [Display(Name="Course Name")] public string? CourseName { get; set; }
+    [Display(Name="Course Description")] public string? CourseDescription { get; set; }
+    [Display(Name="Room Number")] public int RoomNumber { get; set; }
+    [DataType(DataType.Time)] [Display(Name="Start Time")] public TimeOnly StartTime { get; set; }
+    [DataType(DataType.Time)] [Display(Name="End Time")] public TimeOnly EndTime { get; set; }
 }
---
+++ Model/SeedData.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CIS169IntroToNET.Data;
+
+namespace CIS169IntroToNET.Model;
+
+public class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new CIS169IntroToNETContext(
+                   serviceProvider.GetRequiredService<
+                       DbContextOptions<CIS169IntroToNETContext>>()))
+        {
+            if (context == null || context.Course == null)
+            {
+                throw new ArgumentNullException("Null CIS169IntroToNETContext");
+            }
+
+            // Look for any movies.
+            if (context.Course.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Course.AddRange(
+                new Course
+                {
+                    Id = 169,
+                    CourseName = "C#",
+                    CourseDescription = "Find yourself in the teaching of C#. The programming language, this is not a music class.",
+                    RoomNumber = 200,
+                    StartTime = new TimeOnly(9,00),
+                    EndTime = new TimeOnly(9,50)
+                },
+
+                new Course
+                {
+                    Id = 175,
+                    CourseName = "Java II",
+                    CourseDescription = "Learn how to Java, specifically write.",
+                    RoomNumber = 200,
+                    StartTime = new TimeOnly(10,00),
+                    EndTime = new TimeOnly(10,50)
+                },
+
+                new Course
+                {
+                    Id = 412,
+                    CourseName = "COBOL II",
+                    CourseDescription = "Continue learning about the language that most likely manages your money at the ATM",
+                    RoomNumber = 116,
+                    StartTime = new TimeOnly(11,00),
+                    EndTime = new TimeOnly(11,50)
+                },
+
+                new Course
+                {
+                    Id = 501,
+                    CourseName = "Intro to Business Analysis",
+                    CourseDescription = "Teamwork completes the coursework",
+                    RoomNumber = 200,
+                    StartTime = new TimeOnly(8,00),
+                    EndTime = new TimeOnly(8,50)
+                },
+                
+                new Course
+                {
+                    Id = 281,
+                    CourseName = "Mobile Development",
+                    CourseDescription = "Learn how to cram elements neatly on a touchscreen",
+                    RoomNumber = 200,
+                    StartTime = new TimeOnly(13,00),
+                    EndTime = new TimeOnly(13,50)
+                },
+                
+                new Course
+                {
+                    Id = 170,
+                    CourseName = "Jazz Band",
+                    CourseDescription = "Play the note, swing it, and emphasize it. Don't worry, it's not Whiplash.",
+                    RoomNumber = 171,
+                    StartTime = new TimeOnly(17,00),
+                    EndTime = new TimeOnly(18,20)
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
